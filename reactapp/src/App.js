899a1b833c3c59e91f5927f53639c78ed93d072d
Iamneo <techadmin@iamneo.ai>
2023-07-31 16:11:25 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -103,20 +103,7 @@ setShowScore(false);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
